c2db16379ab73b84ca0876df19ff78fa9fcb8e73
raizo <admin@raizo.dev>
2022-11-03 19:54:49 +0100

    fixed config input, changed default values

--- src/config.rs
+++ src/config.rs
@@ -4,13 +4,19 @@ use clap::Parser;
 #[command(author, version)]
 pub struct Configuration {
     // tinkr executeables which should be ran
-    #[arg(long, env, required = true)]
+    #[arg(
+        long,
+        env,
+        required = true,
+        use_value_delimiter = true,
+        value_delimiter = ' '
+    )]
     pub executeables: Vec<String>,
     // amount in minutes
-    #[arg(short, env, default_value = "90")]
+    #[arg(short, env, default_value = "10")]
     pub short_break: u16,
     // amount in minutes
-    #[arg(short, env, default_value = "10")]
+    #[arg(short, env, default_value = "90")]
     pub long_break: u16,
 }
 

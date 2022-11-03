1ab7c628bd4dc566b3e23fc444307cf2358e0fa5
raizo <admin@raizo.dev>
2022-11-03 19:54:28 +0100

    added logging, fixed tinkr_stop

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use dotenvy::dotenv;
 use rand;
 use rand::distributions::Distribution;
 use statrs::distribution::Poisson;
+use std::process::Stdio;
 use std::process::{Child, Command};
 use std::time::Duration;
 
@@ -15,7 +16,8 @@ fn start_tinkr(config: &Configuration, handles: &mut Vec<Child>) -> Result<()> {
     for exe in config.executeables.clone() {
         handles.push(
             Command::new("sh")
-                .args(["-c", &format!("./{}", exe)])
+                .args(["-c", &format!("{}", exe)])
+                .stdout(Stdio::null())
                 .spawn()
                 .context("Failed to create handle to tinkr")?,
         );
@@ -24,13 +26,11 @@ fn start_tinkr(config: &Configuration, handles: &mut Vec<Child>) -> Result<()> {
     Ok(())
 }
 
-fn stop_tinkr() -> Result<()> {
+fn stop_tinkr(handles: &mut Vec<Child>) -> Result<()> {
     log::info!("Stopping all tinkr instances");
-    Command::new("sh")
-        .args(["pkill", "-f", "Wow"])
-        .spawn()
-        .context("Failed to kill wow")?;
-
+    for tinkr in handles {
+        tinkr.kill().context("Failed to kill tinkr instance")?;
+    }
     log::info!("Killed all world of warcraft instances");
 
     Ok(())
@@ -38,6 +38,7 @@ fn stop_tinkr() -> Result<()> {
 
 fn main() -> Result<()> {
     dotenv().ok();
+    pretty_env_logger::init();
 
     let config = config::parse();
     let mut tinkr_handles: Vec<Child> = vec![];
@@ -47,14 +48,14 @@ fn main() -> Result<()> {
     let long = Poisson::new(config.long_break.into()).unwrap();
 
     loop {
-        start_tinkr(&config, &mut tinkr_handles);
+        start_tinkr(&config, &mut tinkr_handles)?;
 
         let next_break = long.sample(&mut r);
         log::info!("Next break will be in {} minutes", next_break);
         std::thread::sleep(Duration::from_secs(60 * next_break as u64));
         let break_length = short.sample(&mut r);
         log::info!("Taking a break for {} minutes", break_length);
-        stop_tinkr();
+        stop_tinkr(&mut tinkr_handles)?;
         std::thread::sleep(Duration::from_secs(60 * break_length as u64))
     }
 }
